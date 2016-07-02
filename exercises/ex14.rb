5e0d22186e09545d5465fa05e97cc2538115cb2d
Cesare Ferrari <cesare@maryandferrari.com>
2016-07-02 09:33:14 -0400

    Add final exercises

---
+++ exercises/ex14.rb
@@ -0,0 +1,18 @@
+contact_data = [
+  ["joe@email.com", "123 Main st.", "555-123-4567"],
+  ["sally@email.com", "404 Not Found Dr.", "123-234-3454"]
+]
+
+contacts = {"Joe Smith" => {}, "Sally Johnson" => {}}
+
+locations = [:email, :address, :phone]
+
+contacts.each do |key, value|
+  locations.each do |location|
+    contacts[key][location] = contact_data.first.first
+    contact_data.first.shift
+  end
+  contact_data.shift
+end
+
+p contacts
---
+++ exercises/ex8.rb
@@ -0,0 +1,5 @@
+h = {cat: "mews", dog: "barks", bird: "tweets"}
+h2 = {:cat => "mews", :dog => "barks", :bird => "tweets"}
+
+p h
+p h2
